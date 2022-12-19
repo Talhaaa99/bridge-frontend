0f7a0316ccde774cc93979eec128e5f7e6dcf492
Talhaaa99 <talha.toufeeq99@gmail.com>
2022-12-19 18:34:28 +0530

    Wallet Styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon, bsc, bscTestnet } from "wagmi/chains";
+
+const ethers = require("ethers");
+const chains = [arbitrum, mainnet, polygon, bsc, bscTestnet];
+
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "c9014e13a272d698a8e1a0d7f21ab60d" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Component {...pageProps} />;
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId={process.env.NEXT_AUTH_PROJECT_ID}
+        ethereumClient={ethereumClient}
+      />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,29 @@
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
 import Dropdown from "../components/Dropdown";
 
-export default function Home() {
+import WalletConnect from "../components/WalletConnect";
+import truncateEthAddress from "truncate-eth-address";
+
+export default function Home(): JSX.Element {
   return (
     <div className="bg-[#F2F2F5] h-screen w-full">
       <header className="flex flex-row justify-between px-20 py-6">
         <h1 className="flex font-bold text-[32px]">Bridge</h1>
-        <button className="flex flex-row border">Wallet Connect</button>
+
+        <WalletConnect />
       </header>
       <form className="h-[1032px] w-[552px] bg-white m-auto rounded-[40px] p-5">
         <h2 className="text-base font-medium leading-8 tracking-wide text-[#02061D]">
           From
         </h2>
-        <div className="grid grid-cols-2 gap-8 bg-[#F1F2F7] px-6 py-4 mt-2 mb-8 rounded-2xl">
-          <div>
+        <div className="grid grid-cols-2 bg-[#F1F2F7] py-4 px-2 mt-2 mb-8 rounded-2xl">
+          <div className="border-r-2 px-4">
             <h2 className="text-base font-medium tracking-wide mb-4">
               Network
             </h2>
             <Dropdown />
           </div>
-          <div>
+          <div className="px-4">
             <h2 className=" text-base font-medium trakcing-wide mb-4">Token</h2>
             <Dropdown />
           </div>
@@ -67,30 +71,38 @@ export default function Home() {
           </p>
         </div>
         {/* info */}
-        <div className="p-4 space-y-4">
+        <div className="p-4 m-2 space-y-4">
           <div className="flex flex-inline  text-base justify-between align-center">
             <p className="self-center">Source:</p>
-            <div className="flex flex-inline space-x-2 justify-center">
-              <p className="font-bold">0x1234567890</p>
-              <img src="./poly.png" className="h-8 w-8"></img>
+            <div className="flex flex-inline align-center gap-x-2 justify-center">
+              <p className="font-bold">
+                {truncateEthAddress(
+                  "0x456d609DF071d620aAfAB68561a65A2A26077100"
+                )}
+              </p>
+              <img src="./poly.png" className="self center h-7 w-7"></img>
             </div>
           </div>
-          <div className="flex flex-inline text-base justify-between  items-center">
+          <div className="flex flex-inline text-base justify-between  align-center">
             <p className="">Destination:</p>
-            <div className="flex items-start space-x-2">
-              <p className="font-bold">0x1234567890</p>
-              <img src="./poly.png" className=" h-8 w-8"></img>
+            <div className="flex flex-inline align-center gap-x-2 justify-center">
+              <p className="font-bold">
+                {truncateEthAddress(
+                  "0x456d609DF071d620aAfAB68561a65A2A26077100"
+                )}
+              </p>
+              <img src="./poly.png" className="self-center h-7 w-7"></img>
             </div>
           </div>
-          <div className="flex flex-row text-base justify-between  items-center">
+          <div className="flex flex-row text-base justify-between  align-center">
             <p className="">Recieving Amount:</p>
             <p className="font-bold">998 USDC</p>
           </div>
-          <div className="flex flex-row text-base justify-between  items-center">
+          <div className="flex flex-row text-base justify-between  align-center">
             <p className="">Fee:</p>
             <p className="font-bold">2 USDC</p>
           </div>
-          <div className="flex flex-row text-base justify-between  items-center">
+          <div className="flex flex-row text-base justify-between  align-center">
             <p className="">Total:</p>
             <p className="font-bold">1000 USDC</p>
           </div>
